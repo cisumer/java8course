fb916fbebdf6595e74a940126a51b3d3c2d890b5
cisumer <cisumer@qq.com>
2020-03-25 16:04:51 +0800

    注释

--- src/java8test/date/DateDemo.java
+++ src/java8test/date/DateDemo.java
@@ -11,10 +11,12 @@ public class DateDemo {
 	LocalDate now_l=LocalDate.now();
 	System.out.println("Date.toString()"+now.toString());
 	System.out.println("LocalDate.toString()"+now_l.toString());
+	
 	System.out.println(now_l.toEpochDay());
 	System.out.println(now_l.getDayOfMonth());
 	
 	System.out.println(now_l.plusDays(-2));
+	//格式化日期
 	System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
 	LocalDateTime.parse("2019-12-02T20:18:23");
